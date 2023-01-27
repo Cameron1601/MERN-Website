88957d0ecf74060743bef6f44b072bf4c7aa09ad
Cameron Thain <113550639+Cameron1601@users.noreply.github.com>
2023-01-27 12:22:24 -0700

    Delete BackEnd directory

--- BackEnd/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isAuthenticated, isAdmin } = require("../middleware/auth");
-const {userProfile } = require("../controllers/auth");
-
-router.get('/admin/isAdmin',  isAuthenticated, isAdmin, userProfile);
-
-module.exports = router;
